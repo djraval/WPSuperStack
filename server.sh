e46a76d2f3964a283331c8b9d7632741b4543463
djraval <devarshi.j.raval@gmail.com>
2017-09-10 00:38:38 +0530

    Master Install Script

--- server.sh
+++ server.sh
@@ -1,4 +1,9 @@
+sudo apt-get update &&
 apt-get install nginx -y &&
 apt-get install php -y &&
-apt-get install -y tmux curl wget nginx php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl &&
-apt-get install -y php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml
+apt-get install -y curl wget nginx php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl &&
+apt-get install -y php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml &&
+wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
+dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
+sudo apt-get update &&
+sudo apt-get install percona-server-server-5.7
