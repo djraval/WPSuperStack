9220fcad21e0a756e6b6ebb0b73dd29386580c5e
djraval <devarshi.j.raval@gmail.com>
2017-09-10 00:46:38 +0530

    Update server.sh
    Added PHP7.1 by Ondrej

--- server.sh
+++ server.sh
@@ -1,5 +1,8 @@
 sudo apt-get update &&
 apt-get install nginx -y &&
+sudo apt-get install -y python-software-properties &&
+sudo add-apt-repository -y ppa:ondrej/php &&
+sudo apt-get update -y &&
 apt-get install php -y &&
 apt-get install -y curl wget nginx php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl &&
 apt-get install -y php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml &&
