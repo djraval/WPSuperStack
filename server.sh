5052f583979a2d30d9f6638fde549ffdc00c5c24
djraval <devarshi.j.raval@gmail.com>
2017-09-09 18:55:53 +0530

    Nginx and PHP extensions Installation

---
+++ server.sh
@@ -0,0 +1,4 @@
+apt-get install nginx -y &&
+apt-get install php -y &&
+apt-get install -y tmux curl wget nginx php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl &&
+apt-get install -y php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml
