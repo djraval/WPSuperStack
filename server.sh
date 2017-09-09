679b0faf6ceed4b56ddda5e4e9c3991034e05d3e
djraval <devarshi.j.raval@gmail.com>
2017-09-10 00:50:16 +0530

    Update server.sh
    apt update(s) reduced

--- server.sh
+++ server.sh
@@ -1,12 +1,11 @@
 sudo apt-get update &&
-apt-get install nginx -y &&
 sudo apt-get install -y python-software-properties &&
 sudo add-apt-repository -y ppa:ondrej/php &&
+wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
+dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
 sudo apt-get update -y &&
+apt-get install nginx -y &&
 apt-get install php -y &&
 apt-get install -y curl wget nginx php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl &&
 apt-get install -y php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml &&
-wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
-dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
-sudo apt-get update &&
 sudo apt-get install percona-server-server-5.7
