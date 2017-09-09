d27677c23ddbdea58b09daeb35ac747fd1d9c191
djraval <devarshi.j.raval@gmail.com>
2017-09-10 00:53:03 +0530

    Update server.sh
    Modified orders of apt installs

--- server.sh
+++ server.sh
@@ -4,8 +4,8 @@ sudo add-apt-repository -y ppa:ondrej/php &&
 wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
 dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
 sudo apt-get update -y &&
+apt-get install -y curl wget &&
 apt-get install nginx -y &&
 apt-get install php -y &&
-apt-get install -y curl wget &&
 apt-get install -y php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml &&
 sudo apt-get install percona-server-server-5.7
