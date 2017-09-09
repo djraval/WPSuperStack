ebbadd9597ab3599225e42fb83927c3f60249298
djraval <devarshi.j.raval@gmail.com>
2017-09-10 00:51:37 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -6,6 +6,6 @@ dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb &&
 sudo apt-get update -y &&
 apt-get install nginx -y &&
 apt-get install php -y &&
-apt-get install -y curl wget nginx php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl &&
-apt-get install -y php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml &&
+apt-get install -y curl wget &&
+apt-get install -y php7.1-fpm php7.1-cli php7.1-curl php7.1-gd php7.1-intl php7.1-mysql php-memcached php7.1-mbstring php7.1-zip php7.1-xml &&
 sudo apt-get install percona-server-server-5.7
